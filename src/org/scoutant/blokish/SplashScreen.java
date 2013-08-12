69b26a7b72b9c297a4e217b709c08d1683304fde
Matt Gardner <mjg82@byu.edu>
2013-08-12 07:57:07 -0400

    Spacing

--- src/org/scoutant/blokish/SplashScreen.java
+++ src/org/scoutant/blokish/SplashScreen.java
@@ -12,36 +12,36 @@ import android.os.Message;
 import android.view.Window;
 
 public class SplashScreen extends Activity {
-	private static final long DELAY = 1500;
+    private static final long DELAY = 1500;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.splashscreen);
-		long delai = DELAY;
-		try {
-			FileInputStream fis;
-			fis = openFileInput("moves.txt");
-			BufferedReader reader = new BufferedReader( new InputStreamReader(fis));
-			if (reader.readLine()!=null && reader.readLine()!=null) {
-				delai = DELAY / 3;
-			}
-		} catch (Exception e) {
-		}
-		handler.sendEmptyMessageDelayed(0, delai);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splashscreen);
+        long delai = DELAY;
+        try {
+            FileInputStream fis;
+            fis = openFileInput("moves.txt");
+            BufferedReader reader = new BufferedReader( new InputStreamReader(fis));
+            if (reader.readLine()!=null && reader.readLine()!=null) {
+                delai = DELAY / 3;
+            }
+        } catch (Exception e) {
+        }
+        handler.sendEmptyMessageDelayed(0, delai);
+    }
 
-	private Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			startActivityForResult(new Intent(SplashScreen.this, UI.class), 0);
-			super.handleMessage(msg);
-		}
-	};
-	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		finish();
-	};
-	
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            startActivityForResult(new Intent(SplashScreen.this, UI.class), 0);
+            super.handleMessage(msg);
+        }
+    };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        finish();
+    };
+
 }
